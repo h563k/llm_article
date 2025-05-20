0d5f30ae85996a923e8ec470f5162133f13045cb
yang <yang>
2025-05-20 22:55:45 +0800

    修复 bug

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ from functionals.multi_process import multi_process_template_model, merge_excel_
 if __name__ == '__main__':
     # 完整模型列表
     template_model_list = [
-        ("yi", "yi-large"),
-        ("qwen", "deepseek-v3"),
-        ("qwen", "deepseek-r1"),
-        ("qwen", "qwq-32b-preview"),
-        ("qwen", "qwen2.5-72b-instruct"),
-        ("kimi", "moonshot-v1-auto"),
-        ("chatglm", "GLM-4-Air-0111"),
-        ("baidu", "ernie-3.5-128k"),
-        ("doubao", "doubao-1-5-pro-32k-250115"),
-        ("openai_wild", "gpt-3.5-turbo"),
-        ("openai_wild", "gpt-4o-mini"),
-        ("openai_wild", "gpt-4o-2024-08-06"),
-        # ("deepseek", "deepseek-chat"),
+        # ("yi", "yi-large"),
+        # ("qwen", "deepseek-v3"),
+        # ("qwen", "deepseek-r1"),
+        # ("qwen", "qwq-32b-preview"),
+        # ("qwen", "qwen2.5-72b-instruct"),
+        # ("kimi", "moonshot-v1-auto"),
+        # ("chatglm", "GLM-4-Air-0111"),
+        # ("baidu", "ernie-3.5-128k"),
+        # ("doubao", "doubao-1-5-pro-32k-250115"),
+        # ("openai_wild", "gpt-3.5-turbo"),
+        # ("openai_wild", "gpt-4o-mini"),
+        # ("openai_wild", "gpt-4o-2024-08-06"),
+        ("deepseek", "deepseek-chat"),
         # 添加更多的 template 和 model 对
     ]
     # # 第一步 数据预处理
